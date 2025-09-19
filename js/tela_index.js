bad180bd77059eede327700b3b4a3db3815270b0
MARCOS PAULO GABLER <31133206+mpgabler@users.noreply.github.com>
2025-09-19 16:11:55 -0300

    acerto no tela_index.js

--- js/tela_index.js
+++ js/tela_index.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/App-Visita-Externa/sw.js')
-      .then((reg) => console.log('Service Worker registrado:', reg))
+    navigator.serviceWorker.register('sw.js')
+      .then((reg) => console.log('Service Worker registrado com sucesso:', reg))
       .catch((err) => console.error('Erro ao registrar Service Worker:', err));
   });
 }
