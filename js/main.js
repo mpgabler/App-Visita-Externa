72f90ad2fe5ee1eb644377b23e4a852eb7fca4d2
MARCOS PAULO GABLER <31133206+mpgabler@users.noreply.github.com>
2025-10-13 13:40:17 -0300

    adcionada mensagem alert de cadastro ok na tela de cadastro

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 import { addCadastro, getCadastro } from "./database.js";
 
-
 //adiciona ouvinte de evento ao formulário
 document
   .getElementById("formulario")
   .addEventListener("submit", async (event) => {
     // prevenir o recarregamento da página
     event.preventDefault(); // Evita o envio padrão do formulário
-
 
     //capturar os valores dos inputs na tela_cadastro.html
     const cadastro = {
@@ -16,34 +14,55 @@ document
       produtor: document.getElementById("produtor").value,
       municipio: document.getElementById("municipio").value,
       telefone_principal: document.getElementById("telefone_principal").value,
-      telefone_contato: document.getElementById("telefone_contato").value || null,
-      inscricao_estadual: document.getElementById("inscricao_estadual").value || null,
+      telefone_contato:
+        document.getElementById("telefone_contato").value || null,
+      inscricao_estadual:
+        document.getElementById("inscricao_estadual").value || null,
 
       // Características da Área
       area_total: parseFloat(document.getElementById("area_total").value) || 0,
-      area_produtiva: parseFloat(document.getElementById("area_produtiva").value) || 0,
-      reserva_legal: document.querySelector('input[name="reserva"]:checked')?.value || "nao",
+      area_produtiva:
+        parseFloat(document.getElementById("area_produtiva").value) || 0,
+      reserva_legal:
+        document.querySelector('input[name="reserva"]:checked')?.value || "nao",
       car: document.querySelector('input[name="car"]:checked')?.value || "nao",
 
       // Comerciais
-      nota_fiscal: document.querySelector('input[name="nota_fiscal"]:checked')?.value || "nao",
-      rastreabilidade: document.querySelector('input[name="rastreabilidade"]:checked') ?.value || "nao",
-      cooperativa: document.querySelector('input[name="cooperativa"]:checked')?.value || "nao",
+      nota_fiscal:
+        document.querySelector('input[name="nota_fiscal"]:checked')?.value ||
+        "nao",
+      rastreabilidade:
+        document.querySelector('input[name="rastreabilidade"]:checked')
+          ?.value || "nao",
+      cooperativa:
+        document.querySelector('input[name="cooperativa"]:checked')?.value ||
+        "nao",
       qual_coop: document.getElementById("textarea_afiliado").value || null,
 
       // Produção e Manejo (pegando tags já adicionadas nos containers)
-      producao: Array.from(document.querySelectorAll("#tagContainerCulturas .tag")).map((tag) => tag.textContent.trim()), 
-      irrigacao: Array.from(document.querySelectorAll("#tagContainerIrrigacao .tag")).map((tag) => tag.textContent.trim()),
-      defensivos: Array.from(document.querySelectorAll("#tagContainerDefensivo .tag")).map((tag) => tag.textContent.trim()),
-      maquinario: Array.from(document.querySelectorAll("#tagContainerMaquinario .tag")).map((tag) => tag.textContent.trim()),
-      beneficiamento: Array.from(document.querySelectorAll("#tagContainerBeneficiamento .tag")).map((tag) => tag.textContent.trim()),
+      producao: Array.from(
+        document.querySelectorAll("#tagContainerCulturas .tag")
+      ).map((tag) => tag.textContent.trim()),
+      irrigacao: Array.from(
+        document.querySelectorAll("#tagContainerIrrigacao .tag")
+      ).map((tag) => tag.textContent.trim()),
+      defensivos: Array.from(
+        document.querySelectorAll("#tagContainerDefensivo .tag")
+      ).map((tag) => tag.textContent.trim()),
+      maquinario: Array.from(
+        document.querySelectorAll("#tagContainerMaquinario .tag")
+      ).map((tag) => tag.textContent.trim()),
+      beneficiamento: Array.from(
+        document.querySelectorAll("#tagContainerBeneficiamento .tag")
+      ).map((tag) => tag.textContent.trim()),
 
       // Políticas Públicas
       politicas_publicas: {
         pnae: document.getElementById("politica_pnae").checked,
         paa: document.getElementById("politica_paa").checked,
         outras: document.getElementById("outras_politicas").checked,
-        qual_politica: document.getElementById("textarea_politicas").value || null,
+        qual_politica:
+          document.getElementById("textarea_politicas").value || null,
       },
 
       // Observações
@@ -54,10 +73,13 @@ document
     };
 
     // Validação simples dos campos obrigatórios (refatorar conforme necessário)
-    if (!cadastro.produtor || !cadastro.propriedade || !cadastro.municipio || !cadastro.telefone_principal) {
-      alert(
-        "Por favor, preencha os campos obrigatórios: "
-      );
+    if (
+      !cadastro.produtor ||
+      !cadastro.propriedade ||
+      !cadastro.municipio ||
+      !cadastro.telefone_principal
+    ) {
+      alert("Por favor, preencha os campos obrigatórios: ");
       return;
     }
 
@@ -70,12 +92,14 @@ document
 
       console.log("Cadastro salvo!");
 
-      // Mostra a mensagem
-      alert("Cadastro Realizado!")
+      // Exibe o alerta no meio da tela
+      alerta.classList.add("mostrar");
 
-      // Redireciona para a tela inicial
-      window.location.href = "index.html";
-
+      // Aguarda 2 segundos e redireciona
+      setTimeout(() => {
+        alerta.classList.remove("mostrar");
+        window.location.href = "index.html";
+      }, 2000);
     } else {
       console.error("Erro ao salvar o cadastro");
     }
